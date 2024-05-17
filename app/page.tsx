317cdf51ae2821faf828cd0608daedf3aac1d439
Scott Trinh <scott@scotttrinh.com>
2024-05-17 10:46:06 -0400

    Do not prefetch built-in UI links
    These are not actual Next.js pages, but rather the auth helper
    library manually routes all /auth requests itself, so having these
    prefetched results in a 404 because Next.js cannot find the page to
    render.
    
    They are redirects anyway, so there is no need to prefetch them.

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default async function Home() {
               <>
                 <Link
                   href={auth.getBuiltinUIUrl()}
+                  prefetch={false}
                   className="text-sm font-semibold leading-6 text-gray-800"
                 >
                   <button className="ring-2 ring-inset ring-primary bg-primarylight px-4 py-2 rounded-md">
@@ -27,6 +28,7 @@ export default async function Home() {
                 </Link>
                 <Link
                   href={auth.getBuiltinUISignUpUrl()}
+                  prefetch={false}
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   <button className="bg-primary px-4 py-2 rounded-md text-white">
