a3194a72f3f8ee9d92e614d20e3cb6a543574b6a
Scott Trinh <scott@scotttrinh.com>
2024-08-27 21:09:46 -0400

    Move TLS security into .env.example

--- edgedb.ts
+++ edgedb.ts
@@ -1,12 +1,7 @@
 import { createClient } from "edgedb";
 import createAuth from "@edgedb/auth-nextjs/app";
 
-export const client = createClient({
-  // Note: when developing locally you will need to set tls security to
-  // insecure, because the development server uses self-signed certificates
-  // which will cause api calls with the fetch api to fail.
-  tlsSecurity: process.env.NODE_ENV === "development" ? "insecure" : undefined,
-});
+export const client = createClient();
 
 export const auth = createAuth(client, {
   baseUrl: "http://localhost:3000",
