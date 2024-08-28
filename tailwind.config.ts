74d309b17da1fd66ebabd906ce662700e50196f4
Scott Trinh <scott@scotttrinh.com>
2024-08-28 11:06:04 -0400

    Fix warning
    Spreading all of the `colors` meant accidentally accessing some
    deprecated colors like `lightBlue`, which emit warnings in the build
    step. Use the `extends` section instead to add these new colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -14,12 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      ...colors,
-      primary: "rgb(14 128 95)",
-      primarylight: "rgb(248 255 253)",
-      primaryhover: "rgb(35 157 122)",
+      colors: {
+        primary: "rgb(14 128 95)",
+        primarylight: "rgb(248 255 253)",
+        primaryhover: "rgb(35 157 122)",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
