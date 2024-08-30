347d66488ab689f819dfda7444aa16e0d2bf5cfd
Yury Selivanov <yury@edgedb.com>
2024-08-30 13:47:15 -0700

    Fix baseUrl

--- edgedb.ts
+++ edgedb.ts
@@ -3,6 +3,13 @@ import createAuth from "@edgedb/auth-nextjs/app";
 
 export const client = createClient();
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+
 export const auth = createAuth(client, {
-  baseUrl: "http://localhost:3000",
+  baseUrl,
 });
