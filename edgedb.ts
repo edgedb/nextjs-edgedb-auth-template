6be04e1d63b8a6db7d3afa01888c26dc6b2a0389
Yury Selivanov <yury@edgedb.com>
2024-08-30 13:58:16 -0700

    More robust URL selection

--- edgedb.ts
+++ edgedb.ts
@@ -3,12 +3,33 @@ import createAuth from "@edgedb/auth-nextjs/app";
 
 export const client = createClient();
 
-const baseUrl =
-  process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+function getBaseUrl() {
+  const vercel_env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  const vercel_url = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const vercel_prod_url = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
+  const local_url = 'http://localhost:3000';
+
+  if (!vercel_env || vercel_env !== 'production') {
+    if (vercel_url) {
+      return vercel_url;
+    }
+
+    return local_url;
+
+  } else {
+    if (vercel_prod_url) {
+      return vercel_prod_url;
+    }
+
+    if (vercel_url) {
+      return vercel_url;
+    }
+
+    return local_url;
+  }
+}
+
+const baseUrl = getBaseUrl();
 
 export const auth = createAuth(client, {
   baseUrl,
