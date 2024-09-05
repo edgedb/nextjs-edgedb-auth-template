909a6755c385609972d5cfc12d6dd3b42559ff48
Scott Trinh <scott@scotttrinh.com>
2024-09-04 22:30:14 -0400

    Add prebuild script
    1. Polls storage until it is ready
    2. Runs migrations
    3. Runs generators

---
+++ prebuild.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+STORAGE_READINESS_POLLING_ATTEMPTS="${STORAGE_READINESS_POLLING_ATTEMPTS:-20}"
+STORAGE_READINESS_POLLING_INTERVAL_SECONDS="${STORAGE_READINESS_POLLING_INTERVAL_SECONDS:-0.5}"
+
+attempt_count=0
+
+while [[ $attempt_count -lt $STORAGE_READINESS_POLLING_ATTEMPTS ]]; do
+    if pnpm exec edgedb instance status -I="$EDGEDB_INSTANCE"; then
+        break
+    fi
+    sleep "$STORAGE_READINESS_POLLING_INTERVAL_SECONDS"
+    ((attempt_count++))
+done
+
+if [[ $attempt_count -eq $STORAGE_READINESS_POLLING_ATTEMPTS ]]; then
+    echo "Maximum attempts ($STORAGE_READINESS_POLLING_ATTEMPTS) reached, could not connect to the storage instance."
+    exit 1
+fi
+
+echo "Storage ready, running migrations and code generation."
+
+pnpm exec edgedb migrate
+pnpm run generate:all
