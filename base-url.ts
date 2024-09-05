aa90cfddccf731439b5250bf48100469c691a2a0
Scott Trinh <scott@scotttrinh.com>
2024-09-05 15:17:57 -0400

    Move baseUrl singleton into separate module

---
+++ base-url.ts
@@ -0,0 +1,22 @@
+let baseUrl: string | null = null;
+
+export function getBaseUrl() {
+  if (baseUrl !== null) {
+    return baseUrl;
+  }
+
+  const isVercel = process.env.VERCEL === "1";
+  if (isVercel) {
+    const vercelEnv = process.env.VERCEL_ENV!;
+    const vercelPreviewUrl = process.env.VERCEL_BRANCH_URL!;
+    const vercelProductionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL!;
+
+    if (vercelEnv === "preview") {
+      baseUrl = `https://${vercelPreviewUrl}`;
+    } else if (vercelEnv === "production") {
+      baseUrl = `https://${vercelProductionUrl}`;
+    }
+  }
+
+  return baseUrl;
+}
--- edgedb.ts
+++ edgedb.ts
@@ -1,23 +1,10 @@
 import { createClient } from "edgedb";
 import createAuth from "@edgedb/auth-nextjs/app";
 
+import { getBaseUrl } from "./base-url";
+
 export const client = createClient();
 
-let baseUrl = "http://localhost:3000";
-
-const isVercel = process.env.VERCEL === "1";
-if (isVercel) {
-  const vercelEnv = process.env.VERCEL_ENV!;
-  const vercelPreviewUrl = process.env.VERCEL_BRANCH_URL!;
-  const vercelProductionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL!;
-
-  if (vercelEnv === "preview") {
-    baseUrl = `https://${vercelPreviewUrl}`;
-  } else if (vercelEnv === "production") {
-    baseUrl = `https://${vercelProductionUrl}`;
-  }
-}
-
 export const auth = createAuth(client, {
-  baseUrl,
+  baseUrl: getBaseUrl(),
 });
