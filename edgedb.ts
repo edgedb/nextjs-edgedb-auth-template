95d34513398970ef6607938cdb8a54478b5bdf82
Scott Trinh <scott@scotttrinh.com>
2024-09-04 22:29:08 -0400

    Set baseUrl dynamically for Vercel (#14)

--- edgedb.ts
+++ edgedb.ts
@@ -3,6 +3,21 @@ import createAuth from "@edgedb/auth-nextjs/app";
 
 export const client = createClient();
 
+let baseUrl = "http://localhost:3000";
+
+const isVercel = process.env.VERCEL === "1";
+if (isVercel) {
+  const vercelEnv = process.env.VERCEL_ENV!;
+  const vercelPreviewUrl = process.env.VERCEL_BRANCH_URL!;
+  const vercelProductionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL!;
+
+  if (vercelEnv === "preview") {
+    baseUrl = `https://${vercelPreviewUrl}`;
+  } else if (vercelEnv === "production") {
+    baseUrl = `https://${vercelProductionUrl}`;
+  }
+}
+
 export const auth = createAuth(client, {
-  baseUrl: "http://localhost:3000",
+  baseUrl,
 });
