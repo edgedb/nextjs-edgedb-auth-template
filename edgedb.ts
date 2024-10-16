fadb7ea3d2c508818ce84bae85263ba8debca2ea
beerose <aleksandra@edgedb.com>
2024-10-16 21:55:51 +0200

    Update tlsSecurity setting

--- edgedb.ts
+++ edgedb.ts
@@ -4,7 +4,7 @@ import createAuth from "@edgedb/auth-nextjs/app";
 import { getBaseUrl } from "./base-url";
 
 export const client = createClient({
-  tlsSecurity: process.env.NODE_ENV === "production" ? "default" : "insecure",
+  tlsSecurity: process.env.NODE_ENV === "development" ? "insecure" : undefined,
 });
 
 export const auth = createAuth(client, {
